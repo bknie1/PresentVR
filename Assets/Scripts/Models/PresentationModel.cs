c7bd82e1bc4600ddc06a29704f23afc0b469254f
bknie1 <brandonknieriem26@gmail.com>
2018-04-17 17:03:39 -0400

    Rearranged image resources, completed image resource load logic.

--- Assets/Scripts/Models/PresentationModel.cs
+++ Assets/Scripts/Models/PresentationModel.cs
@@ -7,9 +7,9 @@ using UnityEngine.UI;
 public class PresentationModel {
 
 	private int currentSlide;
-	private Object[] resources; // Stores our slide pictures (*.PNG, probably)
-	private string presentationDir = "Assets\\Images\\Presentation";
-	private string presentationPlaceholderDir = "Assets\\Images\\Presentation\\ERROR";
+	private Object[] slides; // Stores our slide pictures (*.PNG, probably)
+	private string presentationDir = "Images\\Presentation";
+	private string presentationPlaceholderDir = "Images\\Presentation\\ERROR";
 	private string imageType = "*.png";
 	//-----------------------------------------------------------------------------------
 	public PresentationModel()
@@ -19,23 +19,24 @@ public class PresentationModel {
 		Debug.Log("PM: Loading slides.");
 		try
 		{
-			// directoryTrace(presentationDir, imageType); // DEBUG
-
 			// Import images from directory for global use.
-			this.resources = Resources.LoadAll(presentationDir, typeof(Texture));
+			slides = Resources.LoadAll(presentationDir, typeof(Texture));
 
-			Debug.Log("PM: " + resources.Length + " slides loaded from Resources.");
+			foreach(var s in slides)
+			{
+				Debug.Log(s.name);
+			}
+
+			Debug.Log("PM: " + slides.Length + " slides loaded from Resources.");
 		} catch(MissingReferenceException mre) { Debug.Log (mre.StackTrace); }
 		//--------------------------------------------------------------------------------------
 		// Loads placeholder if our initial load yields no data. Included in APK, so should exist!
-		if (resources.Length <= 0)
+		if (slides.Length <= 0)
 		{
-			Debug.Log("PM: Missing slides! Slide Count: " + resources.Length);
+			Debug.Log("PM: Missing slides! Slide Count: " + slides.Length);
 			try
 			{
-				// directoryTrace(presentationPlaceholderDir, imageType); // DEBUG
-
-				this.resources = Resources.LoadAll(presentationPlaceholderDir, typeof(Texture));
+				this.slides = Resources.LoadAll(presentationPlaceholderDir, typeof(Texture2D));
 				Debug.Log("PM: Loaded placeholder slide.");
 			} catch(MissingReferenceException mre) { Debug.Log (mre.StackTrace); }
 		}
@@ -58,7 +59,7 @@ public class PresentationModel {
 	public void nextSlide()
 	{
 		Debug.Log("PM: Next slide.");
-		if(currentSlide < resources.Length) ++currentSlide;
+		if(currentSlide < slides.Length) ++currentSlide;
 	}
 	//-----------------------------------------------------------------------------------
 	public void previousSlide()
@@ -70,6 +71,6 @@ public class PresentationModel {
 	public Object getCurrentSlide()
 	{
 		Debug.Log("PM: Returning current slide.");
-		return resources [currentSlide]; // Returns the current slide image.
+		return slides [currentSlide]; // Returns the current slide image.
 	}
 }
